b03b33a6910d00d6d9dbaf801072ee18424f3fc7
NamjaJr <hanruslan144@gmail.com>
2024-03-15 19:06:30 +0600

    dz4

--- homeworks/DZ4_Ruslan_36-01/app.js
+++ homeworks/DZ4_Ruslan_36-01/app.js
@@ -1,15 +1,15 @@
-// var tags = ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1',  'p', 'h1', 'ol', 'br'];
-// var tagCount = {}
-//
-// for (let tag of tags){
-//     if (tagCount[tag]) {
-//         tagCount[tag]++
-//     }else{
-//         tagCount[tag] = 1
-//     }
-// }
-//
-// console.log(tagCount)
+var tags = ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1',  'p', 'h1', 'ol', 'br'];
+var tagCount = {}
+
+for (let tag of tags){
+    if (tagCount[tag]) {
+        tagCount[tag]++
+    }else{
+        tagCount[tag] = 1
+    }
+}
+
+console.log(tagCount)
 
 function averageOfPositivNumbers(arr){
     let sum = 0
@@ -29,3 +29,36 @@ function averageOfPositivNumbers(arr){
 
 var numbers = [2, -78, 45,4,-7,-6,-10, 26, 30, 8]
 console.log(averageOfPositivNumbers(numbers))
+
+
+
+
+function sumOfPositiveSquares(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            const square = arr[i] * arr[i];
+            sum += square;
+        }
+    }
+    return sum;
+}
+const array = [1, -2, 3, -4, 5, 6, -7];
+const result = sumOfPositiveSquares(array);
+console.log("Сумма квадратов положительных чисел:", result);
+
+
+const words = ['text', 'frontend', 'apple', 'table', 'tea', 'JavaScript'];
+function filterWordsWithTAndA(words) {
+    const filteredWords = [];
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i].toLowerCase();
+        if (word.includes('t') && word.includes('a')) {
+            filteredWords.push(words[i]);
+        }
+    }
+    return filteredWords;
+}
+const result1 = filterWordsWithTAndA(words);
+console.log(result1);
